23c2dbc7851a3f2f4a2535e59b983a81107076af
Ben Lubar <ben.lubar@gmail.com>
2017-04-13 13:31:31 -0500

    Fix LfsFilter failing when credentials are needed to access the repository.

--- Git/Common/Clients/LibGitSharp/LfsFilter.cs
+++ Git/Common/Clients/LibGitSharp/LfsFilter.cs
@@ -1,15 +1,19 @@
-﻿using LibGit2Sharp;
-using System;
+﻿using Inedo.ExecutionEngine.Executer;
+using LibGit2Sharp;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
-using Inedo.ExecutionEngine.Executer;
+using System;
+using Inedo.IO;
 
 namespace Inedo.Extensions.Clients.LibGitSharp
 {
     internal sealed class LfsFilter : Filter
     {
+        internal readonly List<GitRepositoryInfo> Repositories = new List<GitRepositoryInfo>();
+
         public LfsFilter(string name, IEnumerable<FilterAttributeEntry> attributes) : base(name, attributes)
         {
         }
@@ -26,29 +30,69 @@ namespace Inedo.Extensions.Clients.LibGitSharp
 
         private async Task CleanOrSmudgeAsync(string path, string root, Stream input, Stream output, string mode)
         {
-            using (var process = CreateProcess("git-lfs", $"{mode} -- {path}", root))
+            using (var repo = new Repository(root))
             {
-                var inputCopy = input.CopyToAsync(process.StandardInput.BaseStream);
-                var outputCopy = process.StandardOutput.BaseStream.CopyToAsync(output);
-                var errorRead = process.StandardError.ReadToEndAsync();
+                var originUrl = repo.Network.Remotes["origin"].Url;
 
-                await inputCopy.ConfigureAwait(false);
-                process.StandardInput.Close();
-                await outputCopy.ConfigureAwait(false);
-                process.StandardOutput.Close();
-
-                await process.DelayUntilExitAsync().ConfigureAwait(false);
-
-                var errorText = await errorRead.ConfigureAwait(false);
-
-                if (process.ExitCode == 9009)
+                try
                 {
-                    throw new ExecutionFailureException("git-lfs is not installed on the current server, but it is required by this repository. See https://git-lfs.github.com/ for installation instructions.");
+                    var info = this.GetInfo(root);
+                    repo.Network.Remotes.Update(repo.Network.Remotes["origin"], r => r.Url = info.GetRemoteUrlWithCredentials());
+
+                    using (var process = CreateProcess("git-lfs", $"{mode} -- {path}", root))
+                    {
+                        var inputCopy = input.CopyToAsync(process.StandardInput.BaseStream);
+                        var outputCopy = process.StandardOutput.BaseStream.CopyToAsync(output);
+                        var errorRead = process.StandardError.ReadToEndAsync();
+
+                        await inputCopy.ConfigureAwait(false);
+                        process.StandardInput.Close();
+                        await outputCopy.ConfigureAwait(false);
+                        process.StandardOutput.Close();
+
+                        await process.DelayUntilExitAsync().ConfigureAwait(false);
+
+                        var errorText = await errorRead.ConfigureAwait(false);
+
+                        if (process.ExitCode == 9009)
+                        {
+                            throw new ExecutionFailureException("git-lfs is not installed on the current server, but it is required by this repository. See https://git-lfs.github.com/ for installation instructions.");
+                        }
+                        else if (process.ExitCode != 0)
+                        {
+                            throw new ExecutionFailureException($"git-lfs exited with code {process.ExitCode}\nMessages: {errorText}");
+                        }
+                    }
                 }
-                else if (process.ExitCode != 0)
+                finally
                 {
-                    throw new ExecutionFailureException($"git-lfs exited with code {process.ExitCode}\nMessages: {errorText}");
+                    repo.Network.Remotes.Update(repo.Network.Remotes["origin"], r => r.Url = originUrl);
                 }
+            }
+        }
+
+        private GitRepositoryInfo GetInfo(string root)
+        {
+            var canonicalRoot = PathEx.MakeCanonical(root, '/');
+            lock (this.Repositories)
+            {
+                foreach (var info in this.Repositories)
+                {
+                    var canonicalPath = PathEx.MakeCanonical(info.LocalRepositoryPath, '/');
+                    if (string.Equals(canonicalRoot, canonicalPath, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return info;
+                    }
+
+                    if (canonicalPath.StartsWith(PathEx.EnsureTrailingDirectorySeparator(canonicalRoot), StringComparison.OrdinalIgnoreCase))
+                    {
+                        using (var repo = new Repository(canonicalPath))
+                        {
+                            return new GitRepositoryInfo(new WorkspacePath(root), repo.Network.Remotes["origin"].Url, info.UserName, info.Password);
+                        }
+                    }
+                }
+                throw new InvalidOperationException($"No known repository for path {root}\nKnown paths: {string.Join(", ", this.Repositories.Select(r => r.LocalRepositoryPath))}");
             }
         }
 
--- Git/Common/Clients/LibGitSharp/LibGitSharpClient.cs
+++ Git/Common/Clients/LibGitSharp/LibGitSharpClient.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Inedo.Agents;
 using Inedo.Diagnostics;
 using Inedo.IO;
 using LibGit2Sharp;
@@ -12,10 +11,11 @@ namespace Inedo.Extensions.Clients.LibGitSharp
     public sealed class LibGitSharpClient : GitClient
     {
         private static Task Complete => Task.FromResult<object>(null);
+        private static readonly LfsFilter lfsFilter = new LfsFilter("lfs", new[] { new FilterAttributeEntry("lfs") });
 
         static LibGitSharpClient()
         {
-            GlobalSettings.RegisterFilter(new LfsFilter("lfs", new[] { new FilterAttributeEntry("lfs") }));
+            GlobalSettings.RegisterFilter(lfsFilter);
         }
 
         public LibGitSharpClient(GitRepositoryInfo repository, ILogger log)
@@ -25,53 +25,50 @@ namespace Inedo.Extensions.Clients.LibGitSharp
 
         public override Task CloneAsync(GitCloneOptions options)
         {
-            this.log.LogDebug($"Cloning '{this.repository.RemoteRepositoryUrl}' into '{this.repository.LocalRepositoryPath}'...");
-            this.log.LogDebug("Clone options: " + options);
-            Repository.Clone(
-                this.repository.RemoteRepositoryUrl,
-                this.repository.LocalRepositoryPath,
-                new CloneOptions
-                {
-                    BranchName = options.Branch,
-                    CredentialsProvider = this.CredentialsHandler,
-                    RecurseSubmodules = options.RecurseSubmodules
-                }
-            );
+            try
+            {
+                this.BeginOperation();
 
-            return Complete;
+                this.log.LogDebug($"Cloning '{this.repository.RemoteRepositoryUrl}' into '{this.repository.LocalRepositoryPath}'...");
+                this.log.LogDebug("Clone options: " + options);
+                Repository.Clone(
+                    this.repository.RemoteRepositoryUrl,
+                    this.repository.LocalRepositoryPath,
+                    new CloneOptions
+                    {
+                        BranchName = options.Branch,
+                        CredentialsProvider = this.CredentialsHandler,
+                        RecurseSubmodules = options.RecurseSubmodules
+                    }
+                );
+
+                return Complete;
+            }
+            finally
+            {
+                this.EndOperation();
+            }
         }
 
         public override Task<IEnumerable<string>> EnumerateRemoteBranchesAsync()
         {
-            this.log.LogDebug("Enumerating remote branches...");
-
-            if (!Repository.IsValid(this.repository.LocalRepositoryPath))
+            try
             {
-                this.log.LogDebug($"Repository not found at '{this.repository.LocalRepositoryPath}'...");
-                if (DirectoryEx.Exists(this.repository.LocalRepositoryPath))
+                this.BeginOperation();
+
+                this.log.LogDebug("Enumerating remote branches...");
+
+                if (!Repository.IsValid(this.repository.LocalRepositoryPath))
                 {
-                    var contents = DirectoryEx.GetFileSystemInfos(this.repository.LocalRepositoryPath, MaskingContext.Default);
-                    if (contents.Count > 0)
-                        throw new InvalidOperationException("Specified local repository path is invalid.");
-                }
+                    this.log.LogDebug($"Repository not found at '{this.repository.LocalRepositoryPath}'...");
+                    if (DirectoryEx.Exists(this.repository.LocalRepositoryPath))
+                    {
+                        var contents = DirectoryEx.GetFileSystemInfos(this.repository.LocalRepositoryPath, MaskingContext.Default);
+                        if (contents.Count > 0)
+                            throw new InvalidOperationException("Specified local repository path is invalid.");
+                    }
 
-                var refs = Repository.ListRemoteReferences(this.repository.RemoteRepositoryUrl, this.CredentialsHandler);
-
-                var trimmedRefs = from r in refs
-                                  where r.CanonicalName.StartsWith("refs/heads/")
-                                  let trimmed = r.CanonicalName.Substring("refs/heads/".Length)
-                                  select trimmed;
-
-                return Task.FromResult(trimmedRefs);
-            }
-            else
-            {
-                this.log.LogDebug($"Repository found at '{this.repository.LocalRepositoryPath}'...");
-                using (var repository = new Repository(this.repository.LocalRepositoryPath))
-                {
-                    var origin = repository.Network.Remotes["origin"];
-                    this.log.LogDebug($"Using remote: origin, '{origin.Name}'.");
-                    var refs = repository.Network.ListReferences(origin);
+                    var refs = Repository.ListRemoteReferences(this.repository.RemoteRepositoryUrl, this.CredentialsHandler);
 
                     var trimmedRefs = from r in refs
                                       where r.CanonicalName.StartsWith("refs/heads/")
@@ -80,60 +77,106 @@ namespace Inedo.Extensions.Clients.LibGitSharp
 
                     return Task.FromResult(trimmedRefs);
                 }
+                else
+                {
+                    this.log.LogDebug($"Repository found at '{this.repository.LocalRepositoryPath}'...");
+                    using (var repository = new Repository(this.repository.LocalRepositoryPath))
+                    {
+                        var origin = repository.Network.Remotes["origin"];
+                        this.log.LogDebug($"Using remote: origin, '{origin.Name}'.");
+                        var refs = repository.Network.ListReferences(origin);
+
+                        var trimmedRefs = from r in refs
+                                          where r.CanonicalName.StartsWith("refs/heads/")
+                                          let trimmed = r.CanonicalName.Substring("refs/heads/".Length)
+                                          select trimmed;
+
+                        return Task.FromResult(trimmedRefs);
+                    }
+                }
+            }
+            finally
+            {
+                this.EndOperation();
             }
         }
 
         public override Task<bool> IsRepositoryValidAsync()
         {
-            return Task.FromResult(Repository.IsValid(this.repository.LocalRepositoryPath));
+            try
+            {
+                this.BeginOperation();
+
+                return Task.FromResult(Repository.IsValid(this.repository.LocalRepositoryPath));
+            }
+            finally
+            {
+                this.EndOperation();
+            }
         }
 
         public override Task TagAsync(string tag)
         {
-            this.log.LogDebug($"Using repository at '{this.repository.LocalRepositoryPath}'...");
-            using (var repository = new Repository(this.repository.LocalRepositoryPath))
+            try
             {
-                this.log.LogDebug($"Creating tag '{tag}'...");
-                var createdTag = repository.ApplyTag(tag);
+                this.BeginOperation();
 
-                this.log.LogDebug($"Pushing '{createdTag.CanonicalName}' to remote 'origin'...");
+                this.log.LogDebug($"Using repository at '{this.repository.LocalRepositoryPath}'...");
+                using (var repository = new Repository(this.repository.LocalRepositoryPath))
+                {
+                    this.log.LogDebug($"Creating tag '{tag}'...");
+                    var createdTag = repository.ApplyTag(tag);
 
-                repository.Network.Push(
-                    repository.Network.Remotes["origin"],
-                    createdTag.CanonicalName,
-                    new PushOptions { CredentialsProvider = this.CredentialsHandler }
-                );
+                    this.log.LogDebug($"Pushing '{createdTag.CanonicalName}' to remote 'origin'...");
+
+                    repository.Network.Push(
+                        repository.Network.Remotes["origin"],
+                        createdTag.CanonicalName,
+                        new PushOptions { CredentialsProvider = this.CredentialsHandler }
+                    );
+                }
+
+                return Complete;
             }
-
-            return Complete;
+            finally
+            {
+                this.EndOperation();
+            }
         }
 
         public override Task UpdateAsync(GitUpdateOptions options)
         {
-            this.log.LogDebug($"Using repository at '{this.repository.LocalRepositoryPath}'...");
-            using (var repository = new Repository(this.repository.LocalRepositoryPath))
+            try
             {
-                this.log.LogDebug("Fetching commits from origin...");
-                repository.Fetch("origin", new FetchOptions { CredentialsProvider = CredentialsHandler, Prune = true });
-                var refName = "FETCH_HEAD";
-                if (options.Branch != null)
-                    refName = options.Branch;
-                else if (options.Tag != null)
-                    refName = options.Tag;
-                this.log.LogDebug($"Resetting the index and working tree to {refName}...");
-                repository.Reset(ResetMode.Hard, refName);
-                repository.RemoveUntrackedFiles();
-
-                if (options.RecurseSubmodules)
+                this.log.LogDebug($"Using repository at '{this.repository.LocalRepositoryPath}'...");
+                using (var repository = new Repository(this.repository.LocalRepositoryPath))
                 {
-                    foreach (var submodule in repository.Submodules)
+                    this.log.LogDebug("Fetching commits from origin...");
+                    repository.Fetch("origin", new FetchOptions { CredentialsProvider = CredentialsHandler, Prune = true });
+                    var refName = "FETCH_HEAD";
+                    if (options.Branch != null)
+                        refName = options.Branch;
+                    else if (options.Tag != null)
+                        refName = options.Tag;
+                    this.log.LogDebug($"Resetting the index and working tree to {refName}...");
+                    repository.Reset(ResetMode.Hard, refName);
+                    repository.RemoveUntrackedFiles();
+
+                    if (options.RecurseSubmodules)
                     {
-                        repository.Submodules.Update(submodule.Name, new SubmoduleUpdateOptions { CredentialsProvider = CredentialsHandler, Init = true });
+                        foreach (var submodule in repository.Submodules)
+                        {
+                            repository.Submodules.Update(submodule.Name, new SubmoduleUpdateOptions { CredentialsProvider = CredentialsHandler, Init = true });
+                        }
                     }
                 }
-            }
 
-            return Complete;
+                return Complete;
+            }
+            finally
+            {
+                this.EndOperation();
+            }
         }
 
         public override Task ArchiveAsync(string targetDirectory)
@@ -156,6 +199,22 @@ namespace Inedo.Extensions.Clients.LibGitSharp
                     Username = this.repository.UserName,
                     Password = this.repository.Password
                 };
+            }
+        }
+
+        private void BeginOperation()
+        {
+            lock (lfsFilter.Repositories)
+            {
+                lfsFilter.Repositories.Add(this.repository);
+            }
+        }
+
+        private void EndOperation()
+        {
+            lock (lfsFilter.Repositories)
+            {
+                lfsFilter.Repositories.Remove(this.repository);
             }
         }
     }
