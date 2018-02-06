701e297c99938fa55eef0a1e7d01578db3a741b5
InedoJohn <jrasch@inedo.com>
2018-02-06 11:02:54 -0500

    WIP: Reorganize projects, remove all but InedoExtension

--- Git/Common/VariableFunctions/DefaultGitExePathVariableFunction.cs
+++ Git/Common/VariableFunctions/DefaultGitExePathVariableFunction.cs
@@ -1,18 +1,7 @@
 ﻿using System.ComponentModel;
 using Inedo.Documentation;
-
-#if BuildMaster
-using Inedo.BuildMaster.Extensibility;
-using Inedo.BuildMaster.Extensibility.VariableFunctions;
-using IVariableFunctionContext = Inedo.BuildMaster.Extensibility.IGenericBuildMasterContext;
-#elif Otter
-using Inedo.Otter.Extensibility;
-using Inedo.Otter.Extensibility.VariableFunctions;
-using IVariableFunctionContext = Inedo.Otter.IOtterContext;
-#elif Hedgehog
 using Inedo.Extensibility;
 using Inedo.Extensibility.VariableFunctions;
-#endif
 
 namespace Inedo.Extensions.VariableFunctions
 {
